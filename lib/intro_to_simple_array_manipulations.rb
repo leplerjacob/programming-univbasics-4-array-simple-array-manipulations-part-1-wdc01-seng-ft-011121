e2a0727598f3d4387b1e4a81282b1c899099d925
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-28 16:16:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-# Method uses .push to input element at the end of the given array
+# Method uses .push to input element at the end of the given array and returns the new array
 def using_push(array, element)
   # binding.pry
   
