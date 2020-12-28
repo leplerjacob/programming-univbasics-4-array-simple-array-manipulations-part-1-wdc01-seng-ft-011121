4296786309ec4eb69b7161c151f8dbb3eca64909
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-28 16:20:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,6 +9,7 @@ end
 # Method uses .unshift to input element at start of the given array and returns the new array
 def using_unshift(array, element)
   new_array = array.unshift(element)
+  new_array
 end
   
 # Method uses .pop to remove last element of the given array and returns the "popped" element
