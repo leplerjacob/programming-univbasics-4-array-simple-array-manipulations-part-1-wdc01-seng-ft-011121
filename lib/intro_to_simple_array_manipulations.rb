1c43363c774738ab0b1eaa992d7c297468834b49
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-28 16:17:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,12 +2,7 @@ require 'pry'
 
 # Method uses .push to input element at the end of the given array and returns the new array
 def using_push(array, element)
-  # binding.pry
-  
   array.push(element)
-  
-  # binding.pry
-  # puts array.class
 end
 
 # Method uses .unshift to input element at start of the given array and returns the new array
